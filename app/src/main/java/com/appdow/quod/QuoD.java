c3e61d974624375df14883d4cf1cae4e82b472d5
DEVASHAH <devashah12@gmail.com>
2018-08-27 19:29:37 -0400

    Initial commit

---
+++ app/src/main/java/com/appdow/quod/QuoD.java
@@ -0,0 +1,197 @@
+package com.appdow.quod;
+
+import android.annotation.SuppressLint;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import static com.appdow.quod.Urls.getReturnOneRow;
+
+public class QuoD extends AppCompatActivity {
+    private static String TAG = "QuoD";
+    public static QuoD mAinActivity;
+    private Button copy,share,randomButton;
+    private TextView quoteTextView;
+    public static int random=0,savedRandomNumber=0;
+    public static ClipboardManager clipboard;
+    CoordinatorLayout coordinatorLayout;
+    ArrayList<Integer> list;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quo_d);
+        mAinActivity = this;
+        list = new ArrayList<Integer>();
+        for (int i=1; i<12; i++) {
+            list.add(i);
+        }
+        Collections.shuffle(list);
+
+
+        clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.mainLayout);
+        coordinatorLayout.setBackgroundColor(Color.BLACK);
+
+        copy = (Button) findViewById(R.id.copy);
+        copy.setVisibility( View.GONE);
+        share = (Button) findViewById(R.id.share);
+        randomButton = (Button) findViewById(R.id.random);
+        quoteTextView = (TextView) findViewById(R.id.quoteView);
+        randomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //requestMethod(Integer.toString(randomMethod()));
+                if((list.size() - 1)!= random){
+                requestMethod(Integer.toString(list.get(random)));
+                random++;
+                }
+                else {
+                    random = 0;
+                    Collections.shuffle(list);
+                    requestMethod(Integer.toString(list.get(random)));
+                    random++;
+                }
+                copy.setVisibility( View.VISIBLE);
+            }
+        });//randomButtonClickListener
+
+        copy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ClipData clip = ClipData.newPlainText("quoteCopies", quoteTextView.getText().toString()+"\nFor More quotes checkout QuoD App on Playstore.");
+                clipboard.setPrimaryClip(clip);
+                Snackbar snackbar = Snackbar.make(coordinatorLayout, "Copied!", Snackbar.LENGTH_LONG);
+                snackbar.show();
+            }
+        });//copyClickListerner
+
+
+
+
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                shareButton(quoteTextView.getText().toString());
+            }
+        });//shareClickListerner
+    }
+
+
+
+   public int randomMethod(){
+        savedRandomNumber =random;
+       random = new Random().nextInt(11 - 1) + 1;
+       if(random==savedRandomNumber){
+           randomMethod();
+       }
+       return random;
+   }
+
+
+
+
+
+    public void filessss(){
+
+        Writer writer = null;
+
+        try {
+            writer = new BufferedWriter(new OutputStreamWriter(
+                    new FileOutputStream("filename.txt"), "utf-8"));
+            writer.write("Something");
+        } catch (IOException ex) {
+            // Report
+        } finally {
+            try {writer.close();
+            } catch (Exception ex) {/*ignore*/}
+        }
+
+    }
+    public void requestMethod(final String number){
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, getReturnOneRow(), new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.w(TAG,"gvnvhmvjbk" +response.toString()+random);
+                try {
+                    JSONArray quoteResponse = new JSONArray(response);
+                    JSONObject quoteReponseObject = quoteResponse.getJSONObject(0);
+                    quoteTextView.setText(quoteReponseObject.getString("quote"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Log.w(TAG,  "gvnvhmvjbk" + error);
+
+            }
+        }){
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> header = new HashMap<String, String>();
+                header.put("User-Agent", "QuoD");
+                return header;
+            }
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+
+
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("entry",number);
+                return params;
+            }
+        };
+        MySingleton.getInstance(mAinActivity).addToRequestQueue(stringRequest);
+    }//requestMethod
+
+
+public void shareButton(String shareQuote){
+
+    Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+    sharingIntent.setType("text/plain");
+    String shareBody = shareQuote + "\nFor More quotes checkout QuoD App on Playstore.";
+    sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Subject Here");
+    sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+    startActivity(Intent.createChooser(sharingIntent, "Share via"));
+
+}
+
+}
---
+++ app/src/main/java/com/appdow/quod/Urls.java
@@ -0,0 +1,27 @@
+package com.appdow.quod;
+
+public class Urls {
+
+    public static String insertIntoTable = "https://devashah.000webhostapp.com/connect/insertIntoTable.php";
+    public static String returnOneRow = "https://devashah.000webhostapp.com/connect/returnOneRow.php";
+    public static String getAllTheQuote = "https://devashah.000webhostapp.com/connect/getAllTheQuote.php";
+
+    public static String getInsertIntoTable() {
+        return insertIntoTable;
+    }
+
+    public static String getReturnOneRow() {
+        return returnOneRow;
+    }
+
+    public static String getGetAllTheQuote() {
+        return getAllTheQuote;
+    }
+
+
+
+
+
+
+
+}
