0dd0d3c053ba77aaae00140d3e02daeb7c315785
Deva Shah <dshah@hcn-inc.com>
2018-08-29 15:06:06 -0400

    splashFontAdded

--- app/src/main/java/com/appdow/quod/LogoSplashActivity.java
+++ app/src/main/java/com/appdow/quod/LogoSplashActivity.java
@@ -1,9 +1,12 @@
 package com.appdow.quod;
 
 import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Build;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LogoSplashActivity extends AppCompatActivity {
 private static int SPLASHTIME = 3000;
@@ -11,7 +14,12 @@ private static int SPLASHTIME = 3000;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logo_splash);
-
+        TextView textView = (TextView) findViewById( R.id.textView );
+        Typeface typeface = null;
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+             typeface = getResources().getFont(R.font.pricds);
+        }
+        textView.setTypeface( typeface );
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         new Handler().postDelayed(new Runnable() {
